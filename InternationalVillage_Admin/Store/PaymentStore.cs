4b05d9266cf344fb8e0744e1cacffdda64cdccbb
thanhtam22052001 <72904967+thanhtam22052001@users.noreply.github.com>
2021-12-05 22:19:23 +0700

    view bill for customer

---
+++ InternationalVillage_Admin/Store/PaymentStore.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InternationalVillage_Admin.Model;
+
+namespace InternationalVillage_Admin.Store
+{
+    class PaymentStore
+    {
+        private static PaymentStore instance;
+        internal static PaymentStore Instance { get { if (instance == null) instance = new PaymentStore(); return instance; } set => instance = value; }
+
+
+        private PaymentStore() { }
+        string idCustomer;
+        string nameCustomer;
+        string nameReceptionist;
+        string idBill;
+        string checkin;
+        string checkout;
+        string totalMoney;
+        string status;
+        List<DetailBooking> listDetailBookings;
+
+        public string IdCustomer { get => idCustomer; set => idCustomer = value; }
+        public string IdBill { get => idBill; set => idBill = value; }
+        public string NameCustomer { get => nameCustomer; set => nameCustomer = value; }
+        public string Checkin { get => checkin; set => checkin = value; }
+        public string Checkout { get => checkout; set => checkout = value; }
+        public string NameReceptionist { get => nameReceptionist; set => nameReceptionist = value; }
+        public string TotalMoney { get => totalMoney; set => totalMoney = value; }
+        public string Status { get => status; set => status = value; }
+        internal List<DetailBooking> ListDetailBookings { get => listDetailBookings; set => listDetailBookings = value; }
+
+        public bool FindBill(string idNumber, string visa)
+        {
+            string query = "select * from Customer,Bill where Bill.Id_Customer = Customer.Id_Customer and (Identification = '" + idNumber + "' or Visa = '" + visa + "')";
+            DataTable data = DataProvider.Instance.ExecuteQuery(query);
+            if (data.Rows.Count == 0) return false;
+            foreach (DataRow item in data.Rows)
+            {
+                idCustomer = item["Id_Customer"].ToString();
+                idBill = item["Id_Bill"].ToString();
+                nameCustomer = item["FullName"].ToString();
+                nameReceptionist = AccountStore.Instance.Name;
+                checkin = item["CheckInDate"].ToString();
+                checkout = item["CheckOutDate"].ToString();
+                totalMoney = item["TotalMoney"].ToString();
+                status = item["Status"].ToString();
+
+            }
+
+            ListDetailBookings = new List<DetailBooking>();
+            string query2 = "select * from DetailApartmentBill where ID_Bill = '" + idBill + "'";
+            DataTable data2 = DataProvider.Instance.ExecuteQuery(query2);
+            foreach (DataRow item in data2.Rows)
+            {
+                DetailBooking detailBooking = new DetailBooking(item["Id_Apartment"].ToString(), 1, Int32.Parse(item["Price"].ToString()));
+                ListDetailBookings.Add(detailBooking);
+            }
+
+            string query3 = "select DetailServiceBill.Price , ServiceName, Quantity from Service, DetailServiceBill where Service.ID_Service = DetailServiceBill.ID_Service and ID_Bill = '"+idBill+"'";
+            DataTable data3 = DataProvider.Instance.ExecuteQuery(query3);
+            foreach (DataRow item in data3.Rows)
+            {
+                DetailBooking detailBooking = new DetailBooking(item["ServiceName"].ToString(), Int32.Parse(item["Quantity"].ToString()), Int32.Parse(item["Price"].ToString()));
+                ListDetailBookings.Add(detailBooking);
+            }
+
+            return true;
+
+        }
+       
+
+
+    }
+}
--- InternationalVillage_Admin/ViewModel/PaymentDetailViewModel.cs
+++ InternationalVillage_Admin/ViewModel/PaymentDetailViewModel.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using InternationalVillage_Admin.Store;
+using InternationalVillage_Admin.Model;
+using System.Windows;
 
 
 
@@ -14,14 +16,220 @@ namespace InternationalVillage_Admin.ViewModel
     class PaymentDetailViewModel : BaseViewModel
     {
         public ICommand ShowBank { get; set; }
+        public ICommand ChooseBankChanged { get; set; }
+        public ICommand UnCheckCash { get; set; }
+        public ICommand UnCheckCard { get; set; }
+        public ICommand LoadBillTable { get; set; }
+        public ICommand LoadIDBill { get; set; }
+        public ICommand LoadCustomer { get; set; }
+        public ICommand LoadReceptionist { get; set; }
+        public ICommand LoadCheckInDate { get; set; }
+        public ICommand LoadCheckOutDate { get; set; }
+        public ICommand LoadTotalMoney { get; set; }
+        public ICommand LoadStatus { get; set; }
+        public ICommand HiddenCheckCard { get; set; }
+        public ICommand HiddenCheckCash { get; set; }
+        public ICommand HiddenCBBank { get; set; }
+        public ICommand Payment { get; set; }
+        public ICommand UpdateChangeMoney { get; set; }
+        public ICommand PaidChanged { get; set; }
+        public ICommand HiddenPaidMoney { get; set; }
+        public ICommand HiddenChangeMoney { get; set; }
+        public ICommand CheckPaidByCard { get; set; }
+        public ICommand CheckChangeByCard { get; set; }
+        public int Paid { get => paid; set => paid = value; }
+        public int Change { get => change; set => change = value; }
+        public int Total { get => total; set => total = value; }
+        public bool IsChooseBank { get => isChooseBank; set => isChooseBank = value; }
 
+        int paid = 0;
+        int change = 0;
+        int total = Int32.Parse(PaymentStore.Instance.TotalMoney);
+        string TypePay = "Cash";
+        bool isChooseBank = false;
         public PaymentDetailViewModel()
         {
             ShowBank = new RelayCommand<ComboBox>((p) => { return true; }, (p) =>
             {
-                
-                p.Visibility=Visibility.Visible;
+                if (TypePay.Equals("Cash"))
+                {
+                    p.Visibility = Visibility.Hidden;
+                }
+                else
+                {
+                    p.Visibility = Visibility.Visible;
+                }
             });
+            ChooseBankChanged = new RelayCommand<ComboBox>((p) => { return true; }, (p) =>
+            {
+                if (p.SelectedItem != null) isChooseBank = true;
+            });
+            UnCheckCard = new RelayCommand<CheckBox>((p) => { return true; }, (p) =>
+            {
+                p.IsChecked = false;
+                TypePay = "Cash";
+            });
+            UnCheckCash = new RelayCommand<CheckBox>((p) => { return true; }, (p) =>
+            {
+                p.IsChecked = false;
+                TypePay = "Card";
+            });
+
+            LoadBillTable = new RelayCommand<DataGrid>((p) => { return true; }, (p) =>
+            {
+                p.ItemsSource = PaymentStore.Instance.ListDetailBookings;
+
+            });
+            LoadIDBill = new RelayCommand<TextBlock>((p) => { return true; }, (p) =>
+            {
+                p.Text = PaymentStore.Instance.IdBill;
+                
+
+            });
+            LoadCustomer = new RelayCommand<TextBlock>((p) => { return true; }, (p) =>
+            {
+                p.Text = PaymentStore.Instance.NameCustomer;
+
+
+            });
+            LoadReceptionist = new RelayCommand<TextBlock>((p) => { return true; }, (p) =>
+            {
+                p.Text = PaymentStore.Instance.NameReceptionist;
+
+
+            });
+            LoadCheckInDate = new RelayCommand<TextBlock>((p) => { return true; }, (p) =>
+            {
+                p.Text = DateTime.Parse(PaymentStore.Instance.Checkin).ToShortDateString();
+
+
+            });
+            LoadCheckOutDate = new RelayCommand<TextBlock>((p) => { return true; }, (p) =>
+            {
+                p.Text = DateTime.Parse(PaymentStore.Instance.Checkout).ToShortDateString();
+
+
+            });
+            LoadTotalMoney = new RelayCommand<TextBlock>((p) => { return true; }, (p) =>
+            {
+                p.Text = PaymentStore.Instance.TotalMoney + "$";
+                Total = Int32.Parse(PaymentStore.Instance.TotalMoney);
+
+
+            });
+            LoadStatus = new RelayCommand<TextBlock>((p) => { return true; }, (p) =>
+            {
+                if (!PaymentStore.Instance.Status.Equals("Not accepted yet"))
+                    p.Text = "Paid by "+PaymentStore.Instance.Status;
+                else p.Text = PaymentStore.Instance.Status;
+
+            });
+            HiddenCheckCash = new RelayCommand<CheckBox>((p) => { return true; }, (p) =>
+            {
+                if (!PaymentStore.Instance.Status.Equals("Not accepted yet"))
+                    p.Visibility = Visibility.Hidden;
+
+            });
+            HiddenCheckCard = new RelayCommand<CheckBox>((p) => { return true; }, (p) =>
+            {
+                if (!PaymentStore.Instance.Status.Equals("Not accepted yet"))
+                    p.Visibility = Visibility.Hidden;
+
+            });
+            HiddenCBBank = new RelayCommand<ComboBox>((p) => { return true; }, (p) =>
+            {
+                    p.Visibility = Visibility.Hidden;
+
+            });
+            HiddenPaidMoney = new RelayCommand<TextBox>((p) => { return true; }, (p) =>
+            {
+                if (!PaymentStore.Instance.Status.Equals("Not accepted yet"))
+                    p.Visibility = Visibility.Hidden;
+
+            });
+            HiddenChangeMoney = new RelayCommand<TextBlock>((p) => { return true; }, (p) =>
+            {
+                if (!PaymentStore.Instance.Status.Equals("Not accepted yet"))
+                    p.Visibility = Visibility.Hidden;
+
+            });
+            Payment = new RelayCommand<Button>((p) => { return (paid >= total); }, (p) =>
+            {
+                
+                    string query = "Update Bill set Status= '"+TypePay+"' , Id_Receptionist = '"+AccountStore.Instance.IdUser+"' where ID_Bill = '"+PaymentStore.Instance.IdBill+"'";
+                    int result = DataProvider.Instance.ExecuteNonQuery(query);
+                    if (result != 0)
+                    {
+                        MessageBox.Show("Payment success!");
+                        PaymentStore.Instance.Status = TypePay;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Payment fail!");
+                    }
+                
+            });
+            PaidChanged = new RelayCommand<TextBox>((p) => { return true; }, (p) =>
+            {
+                if (!p.Text.Equals(""))
+                {
+                    try
+                    {
+                        paid = Int32.Parse(p.Text);
+                    }
+                    catch (FormatException e)
+                    {
+                        MessageBox.Show("Paid money must be number!");
+                    }
+                }
+            });
+            CheckPaidByCard = new RelayCommand<TextBox>((p) => { return true; }, (p) =>
+            {
+                if (TypePay.Equals("Card"))
+                {
+                    if (isChooseBank)
+                    {
+                        paid = total;
+                        p.Text = paid.ToString();
+                    }
+                    p.IsReadOnly = true;
+                }
+                else
+                {
+                    paid = 0;
+                    p.Text = "";
+                    p.IsReadOnly = false;
+                }
+                
+            });
+            CheckChangeByCard = new RelayCommand<TextBlock>((p) => { return true; }, (p) =>
+            {
+                if (TypePay.Equals("Card"))
+                {
+                    if (isChooseBank)
+                    {
+                        change = 0;
+                        p.Text = change.ToString();
+                    }
+                }
+                else
+                {
+                    change = 0;
+                    p.Text = "";
+                }
+
+
+            });
+            UpdateChangeMoney = new RelayCommand<TextBlock>((p) => { return true; }, (p) =>
+            {
+                if (paid >= total) p.Text = (paid - total).ToString();
+                else
+                {
+                    p.Text = "Paid money must be greater than or equal to total money!";
+                    
+                }
+            });
+           
         }
     }
 }
