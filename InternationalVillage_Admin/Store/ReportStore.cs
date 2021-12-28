a8a27451f27f2e0afc387e34e39d07af95f438c2
thanhtam22052001 <72904967+thanhtam22052001@users.noreply.github.com>
2021-12-28 15:26:00 +0700

    hjsdhs

---
+++ InternationalVillage_Admin/Store/ReportStore.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternationalVillage_Admin.Store
+{
+    class ReportStore
+    {
+        private static ReportStore instance;
+        internal static ReportStore Instance { get { if (instance == null) instance = new ReportStore(); return instance; } set => instance = value; }
+        private ReportStore() { }
+
+        public DataTable GetPieChartByDay(DateTime start, DateTime due)
+        {
+            
+            string query = "SELECT DetailApartment.TypeOfApartment,  sum(BookingApartmentTemp.Quantity) AS 'Sum'  from DetailApartment left join BookingApartmentTemp "
+                             + " on DetailApartment.TypeOfApartment = BookingApartmentTemp.TypeOfApartment "
+                             + "where State = 'Accepted' and CheckInDate BETWEEN CAST('"+ start.ToString("yyyy-MM-dd H:mm:ss")+"' AS Datetime) AND CAST('"+ due.ToString("yyyy-MM-dd H:mm:ss") + "' AS Datetime) "
+                             + "group by  DetailApartment.TypeOfApartment; ";
+            DataTable data = DataProvider.Instance.ExecuteQuery(query);
+            return data;
+        }
+
+        public DataTable GetPieChartByMonth(int year)
+        {
+
+            string query = "SELECT DetailApartment.TypeOfApartment,  sum(BookingApartmentTemp.Quantity) AS 'Sum'  from DetailApartment left join BookingApartmentTemp "
+                             + " on DetailApartment.TypeOfApartment = BookingApartmentTemp.TypeOfApartment "
+                             + "where State = 'Accepted' and year(CheckInDate) = "+year.ToString()+" "
+                             + "group by  DetailApartment.TypeOfApartment; ";
+            DataTable data = DataProvider.Instance.ExecuteQuery(query);
+            return data;
+        }
+
+        public DataTable GetBarChartByDay(DateTime start, DateTime due)
+        {
+
+            string query = "SELECT Service.ServiceName,  sum(OderingServiceTable.Quantity) AS 'Sum'  from Service left join OderingServiceTable "
+                            + "on Service.Id_Service = OderingServiceTable.Id_Service "
+                            + "where State = 'Accepted' and CheckInDate BETWEEN CAST('" + start.ToString("yyyy-MM-dd H:mm:ss") + "' AS Datetime) AND CAST('" + due.ToString("yyyy-MM-dd H:mm:ss") + "' AS Datetime) "
+                            + "group by  Service.ServiceName; ";
+            DataTable data = DataProvider.Instance.ExecuteQuery(query);
+            return data;
+        }
+        public DataTable GetBarChartByMonth(int year)
+        {
+
+            string query = "SELECT Service.ServiceName,  sum(OderingServiceTable.Quantity) AS 'Sum'  from Service left join OderingServiceTable "
+                            + "on Service.Id_Service = OderingServiceTable.Id_Service "
+                            + "where State = 'Accepted' and year(CheckInDate) = " + year.ToString() + " "
+                            + "group by  Service.ServiceName; ";
+            DataTable data = DataProvider.Instance.ExecuteQuery(query);
+            return data;
+        }
+
+        public int[,] GetLineChart(int year)
+        {
+            int[,] result = new int[3, 13];
+            for (int i =0; i < 3; i++)
+               for (int j = 1; j <13; j++)
+                {
+                    result[i, j] = 0;
+                }
+
+            string query = "SELECT month(DetailApartmentBill.CheckInDate) as 'Month',  sum(DetailApartmentBill.Price) AS 'Sum'  from DetailApartmentBill "
+                           + " where year(DetailApartmentBill.CheckInDate) = "+year+" and DetailApartmentBill.Id_Apartment like '3A%' "
+                           +  " group by  month(DetailApartmentBill.CheckInDate); ";
+            DataTable data = DataProvider.Instance.ExecuteQuery(query);
+
+            foreach (DataRow row in data.Rows)
+            {
+                result[0, Int32.Parse(row["Month"].ToString())] = Int32.Parse(row["Sum"].ToString());
+            }
+////////////////
+            string query1 = "SELECT month(DetailApartmentBill.CheckInDate) as 'Month',  sum(DetailApartmentBill.Price) AS 'Sum'  from DetailApartmentBill "
+                           + " where year(DetailApartmentBill.CheckInDate) = " + year + " and DetailApartmentBill.Id_Apartment like '3B%' "
+                           + " group by  month(DetailApartmentBill.CheckInDate); ";
+            DataTable data1 = DataProvider.Instance.ExecuteQuery(query1);
+
+            foreach (DataRow row in data1.Rows)
+            {
+                result[1, Int32.Parse(row["Month"].ToString())] = Int32.Parse(row["Sum"].ToString());
+            }
+////////////////
+            string query2 = "SELECT month(DetailApartmentBill.CheckInDate) as 'Month',  sum(DetailApartmentBill.Price) AS 'Sum'  from DetailApartmentBill "
+                           + " where year(DetailApartmentBill.CheckInDate) = " + year + " and DetailApartmentBill.Id_Apartment like '2A%' "
+                           + " group by  month(DetailApartmentBill.CheckInDate); ";
+            DataTable data2 = DataProvider.Instance.ExecuteQuery(query2);
+
+            foreach (DataRow row in data2.Rows)
+            {
+                result[2, Int32.Parse(row["Month"].ToString())] = Int32.Parse(row["Sum"].ToString());
+            }
+
+            return result;
+        }
+    }
+}
---
+++ InternationalVillage_Admin/ViewModel/ReportPageViewModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+using InternationalVillage_Admin.Store;
+using InternationalVillage_Admin.Model;
+using InternationalVillage_Admin.Pages;
+using System.Windows;
+using LiveCharts.Wpf;
+using LiveCharts;
+using LiveCharts.Defaults;
+
+namespace InternationalVillage_Admin.ViewModel
+{
+    class ReportPageViewModel
+    {
+        public ICommand LoadPieChart { get; set; }
+
+        private SeriesCollection seriesCollection { get; set; }
+        public Func<ChartPoint, string> PointLabel { get; set; }
+        public ReportPageViewModel()
+        {
+            LoadPieChart = new RelayCommand<PieChart>((p) => { return true; }, (p) =>
+            {
+                SetSeriesCollection(10, 10, 20);
+                p.Series = seriesCollection;
+                PointLabel = chartPoint =>
+                string.Format("{0} ({1:P})", chartPoint.Y, chartPoint.Participation);
+                
+                MessageBox.Show("alo");
+            });
+        }
+
+        public void SetSeriesCollection(int Lu, int High, int Stand)
+        {
+            seriesCollection = new SeriesCollection
+            {
+                new PieSeries
+                {
+                    Title="Luxury",
+                    Values = new ChartValues<ObservableValue> {new ObservableValue(Lu)},
+                    DataLabels = true
+                },
+                new PieSeries
+                {
+                    Title="High Standard",
+                    Values = new ChartValues<ObservableValue> {new ObservableValue(High)},
+                    DataLabels = true
+                },
+                new PieSeries
+                {
+                    Title="Standard",
+                    Values = new ChartValues<ObservableValue> {new ObservableValue(Stand)},
+                    DataLabels = true
+                }
+            };
+        }
+    }
+}
